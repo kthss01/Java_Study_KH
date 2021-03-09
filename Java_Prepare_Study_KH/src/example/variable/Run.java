58d29f706adcfdfe42fe08ee807dd2db26e5fa1f
kthss01 <kthss01@naver.com>
2021-03-09 18:44:19 +0900

    자바과정 예비수강생 기초학습 2일차 자바 기초 3회 상수

--- Java_Prepare_Study_KH/src/example/variable/Run.java
+++ Java_Prepare_Study_KH/src/example/variable/Run.java
@@ -5,7 +5,12 @@ public class Run {
 	public static void main(String[] args) {
 		Variable var = new Variable();
 //		var.initVariable();
-		var.changeValue();
+//		var.changeValue();
+//		var.testFinal();
+//		var.testOverFlow();
+//		var.testCasting();
+//		var.testCasting2();
+		var.testDataLoss();
 	}
 
 }
--- Java_Prepare_Study_KH/src/example/variable/Variable.java
+++ Java_Prepare_Study_KH/src/example/variable/Variable.java
@@ -6,65 +6,65 @@ public class Variable {
 
 	public void initVariable() {
 		// 변수 선언과 동시에 초기화
-		
+
 		// 논리형 : 1 byte
 		boolean isTrue = true;
 		boolean isFalse = false;
-		
+
 		// 문자 : 2 byte
 		char ch = 'a'; // '' 싱글 쿼테이션 사용
 		// char ch2= 'bc'; // 하나의 문자만 대입 가능
-		
+
 		// 문자열 : 참조형 (주소값 저장)
 		String str = "안녕하세요"; // "" 더블 쿼테이션 사용
-		
+
 		// 숫자형
 		// 정수
 		byte bnum = 1; // 1 byte
 		short snum = 2; // 2 byte
 		int inum = 4; // 4 byte
 		long lnum = 8; // 8 byte
-		
+
 		// 실수
 		float fnum = 4.0f; // 4 byte
 		double dnum = 8.0; // 8 byte
-		
+
 		// 변수에 저장된 값 출력하기
 		System.out.println("isTrue의 값 : " + isTrue);
 		System.out.println("isFalse의 값 : " + isFalse);
-		
+
 		System.out.println("ch의 값 : " + ch);
 		System.out.println("str의 값 : " + str);
-		
+
 		System.out.println("bnum의 값 : " + bnum);
 		System.out.println("snum의 값 : " + snum);
 		System.out.println("inum의 값 : " + inum);
 		System.out.println("lnum의 값 : " + lnum);
-		
+
 		System.out.println("fnum의 값 : " + fnum);
 		System.out.println("dnum의 값 : " + dnum);
 	}
-	
+
 	public void changeValue() {
 		// 변수값 변경 테스트
 		String name;
 		char gender;
 		int age;
 		double height;
-		
+
 		name = "김영희";
 		gender = 'F';
 		age = 20;
 		height = 171.5;
-		
+
 		System.out.println(name + "님의 개인정보");
 		System.out.println("gender : " + gender);
 		System.out.println("age : " + age);
 		System.out.println("height : " + height);
-		
+
 		// 키보드로 값을 입력받아 변수에 저장된 값 변경
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("이름을 입력하세요 : ");
 		name = sc.nextLine();
 		System.out.println("성별을 입력하세요 : ");
@@ -73,10 +73,120 @@ public class Variable {
 		age = sc.nextInt();
 		System.out.println("키를 입력하세요 : ");
 		height = sc.nextDouble();
-		
+
 		System.out.println(name + "님의 개인정보");
 		System.out.println("gender : " + gender);
 		System.out.println("age : " + age);
 		System.out.println("height : " + height);
+	}
+
+	public void testFinal() {
+		// 상수 테스트
+		int size; // 변수
+		final int SIZE; // 상수
+
+		size = 10;
+		SIZE = 10;
+
+		System.out.println("==== 값 변경 전 ===");
+		System.out.println("size : " + size);
+		System.out.println("SIZE : " + SIZE);
+
+		// 변수의 값 변경
+		size = 20;
+//		SIZE = 20; // 상수는 초기화 이후 다른 데이터 대입 불가
+
+		System.out.println("==== 값 변경 후 ===");
+		System.out.println("size : " + size);
+		System.out.println("SIZE : " + SIZE);
+	}
+
+	public void testOverFlow() {
+		// 데이터 오버플로우 테스트
+		System.out.println("byte의 최대값 : " + Byte.MAX_VALUE);
+		System.out.println("byte의 최소값 : " + Byte.MIN_VALUE);
+
+//		byte bnum = 128; // 저장 가능 범위를 벗어나므로 에러 발생
+		byte bnum = 127;
+		bnum += 1;
+		System.out.println("byte의 최대값 + 1 : " + bnum);
+
+		bnum = -128;
+		bnum -= 1;
+		System.out.println("byte의 최소값 - 1 : " + bnum);
+
+		int num1 = 100000;
+		int num2 = 30000;
+
+		// int의 저장 가능 범위는 약 21억이므로 30억이라는 값을 대입하면 오버플로우 발생
+//		int result = num1 * num2; // 예상 값 30억
+		long result = (long) num1 * num2;
+		System.out.println("계산 결과 : " + result);
+	}
+
+	public void testCasting() {
+		// 형변환 테스트
+		boolean bool = true;
+//		bool = 1; // 형변환 불가
+
+		// char -> int : 자동 형변환
+		int num = 'A';
+		System.out.println("num : " + num);
+
+		// char에 정수값 저장 가능 (숫자에 해당하는 유니코드 문자)
+		char ch = 66;
+		System.out.println("ch : " + ch);
+//		ch = -66; // 음수는 대입 불가능
+
+		// int -> char : 강제 형변환
+		char ch2 = (char) num;
+		System.out.println("ch2 : " + ch2);
+	}
+
+	public void testCasting2() {
+		// int와 long의 연산
+		int inum = 4;
+		long lnum = 8;
+
+		// 1. int 결과
+		int isum = (int) (inum + lnum);
+		isum = inum + (int) lnum;
+		System.out.println("isum : " + isum);
+
+		// 2. long 결과
+		long lsum = inum + lnum;
+		System.out.println("lsum : " + lsum);
+
+		// byte, short의 연산 결과 : int
+		byte bnum = 1;
+		short snum = 2;
+
+		int sum1 = bnum + snum;
+		System.out.println("sum1 : " + sum1);
+
+		// byte or short의 결과를 얻기 위해 강제 형변환
+		short sum2 = (short) (bnum + snum);
+		System.out.println("sum2 : " + sum2);
+	}
+
+	public void testDataLoss() {
+		// 데이터 손실 테스트
+
+		// 정수 -> 실수
+		long lnum = 100;
+		float fnum = lnum;
+		System.out.println("fnum : " + fnum);
+
+		// 실수 -> 정수
+		// 소수점 이하를 절삭하기 때문에 데이터 손실
+		double dnum = 10.5;
+		int inum = (int) dnum;
+		System.out.println("inum : " + inum);
+
+		int num = 290;
+		System.out.println("num : " + num);
+
+		byte bnum = (byte) num;
+		System.out.println("bnum : " + bnum);
 	}
 }
