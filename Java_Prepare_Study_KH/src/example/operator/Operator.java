dfd210155159bf2188906f773c6d0d1429ca6649
kthss01 <kthss01@naver.com>
2021-03-10 20:34:47 +0900

    자바과정 예비수강생 기초학습 3일차
    자바 기초 5회 연산자

---
+++ Java_Prepare_Study_KH/src/example/operator/Operator.java
@@ -0,0 +1,76 @@
+package example.operator;
+
+import java.util.Scanner;
+
+public class Operator {
+	public void operatorTest() {
+		// 증감 연산자 연습학
+		int a = 5;
+		int b = 10;
+
+		int c = (++a) + b; // a:6 b:10 c:16
+		int d = c / a; // d:2
+		int e = c % a; // a:6 e:4
+		int f = e++; // f:4 e:5
+		int g = (--b) + (d--); // b:9 g:11 d:1
+		int h = 2;
+		h *= e; // h:10
+
+		System.out.println("a = " + a); // 6
+		System.out.println("b = " + b); // 9
+		System.out.println("c = " + c); // 16
+		System.out.println("d = " + d); // 1
+		System.out.println("e = " + e); // 5
+		System.out.println("f = " + f); // 4
+		System.out.println("g = " + g); // 11
+		System.out.println("h = " + h); // 10
+	}
+
+	public void operatorTest2() {
+		// 산술 연산자 % : 나누었을 때 나머지를 구하는 연산자
+		// 짝수 -> 값 % 2 == 0
+		// 홀수 -> 값 % 2 == 1
+		int num1 = 10;
+		int num2 = 3;
+		System.out.println("num1 % num2 = " + (num1 % num2)); // 1
+
+		// 논리 연산자
+		// 1과 100 사이의 수
+		// 1 < 수 < 10 (x)
+		// 변수 > 1 && 변수 < 100
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수 입력 : ");
+		int num3 = sc.nextInt();
+
+		System.out.println("1과 100 사이의 수 ? " + (num3 > 1 && num3 < 100));
+
+		// 복합 대입 연산자
+		// 코드가 간결하며 메모리에 직접 연산을 수행하여 연산속도도 빠름
+		int num4 = 5;
+		num4 += 10;
+		System.out.println("num4 : " + num4); // 15
+
+		num4 -= 10;
+		System.out.println("num4 : " + num4); // 5
+
+		num4 *= 10;
+		System.out.println("num4 : " + num4); // 50
+
+		num4 /= 10;
+		System.out.println("num4 : " + num4); // 5
+
+		num4 %= 10;
+		System.out.println("num4 : " + num4); // 5
+	}
+	
+	public void operatorTest3() {
+		// 삼항 연산자
+		// 사용자에 값을 하나 받아서 그 값이 양수인지 음수인지 0인지 판별
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수 하나 입력 : ");
+		int num = sc.nextInt();
+		
+		String result = num > 0 ? "양수" : num == 0 ? "0" : "음수";
+		System.out.println(result);
+	}
+}
---
+++ Java_Prepare_Study_KH/src/example/operator/Run.java
@@ -0,0 +1,12 @@
+package example.operator;
+
+public class Run {
+
+	public static void main(String[] args) {
+		Operator op = new Operator();
+//		op.operatorTest();
+//		op.operatorTest2();
+		op.operatorTest3();
+	}
+
+}
